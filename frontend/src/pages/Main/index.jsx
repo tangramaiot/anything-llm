bc6ff1f70e1c6623cf462bd18fd74370f7095a0a
rong.tsai <jhengrong.tsai@gmail.com>
2024-12-26 11:00:51 +0800

    feat: Add mobile sidebar

--- frontend/src/pages/Main/index.jsx
+++ frontend/src/pages/Main/index.jsx
@@ -1,6 +1,6 @@
 import React, { lazy } from "react";
 import DefaultChatContainer from "@/components/DefaultChat";
-import Sidebar from "@/components/Sidebar";
+import Sidebar, { SidebarMobileHeader} from "@/components/Sidebar";
 import PasswordModal, { usePasswordModal } from "@/components/Modals/Password";
 import { isMobile } from "react-device-detect";
 import { FullScreenLoader } from "@/components/Preloader";
@@ -20,6 +20,7 @@ export default function Main() {
     <div className="w-screen h-screen overflow-hidden bg-primary flex">
       {!isMobile && <Sidebar />} 
       <div className="absolute top-3 md:top-6 md:right-10 w-full h-fit z-99">
+        {isMobile && <SidebarMobileHeader />}
         <FunctionalMenu />
       </div>
       <div className="transition-all duration-500 md:ml-[2px] md:mr-[16px] md:my-[16px] md:rounded-[16px] w-screen border-2 border-outline bg-chat-gradient">
